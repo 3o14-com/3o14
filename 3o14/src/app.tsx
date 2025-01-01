da2f2837c74d77aab2a42dfb0344c6ec80457637
faultypointer <faultypointer@proton.me>
2025-01-01 21:29:39 +0545

    setup get and post routes

--- 3o14/src/app.tsx
+++ 3o14/src/app.tsx
@@ -2,12 +2,46 @@ import { Hono } from "hono";
 import { federation } from "@fedify/fedify/x/hono";
 import { getLogger } from "@logtape/logtape";
 import fedi from "./federation.ts";
+import db from "./db.ts";
+import type { User } from "./schema.ts";
+
+import { Layout, SetupForm } from "./views.tsx";
 
 const logger = getLogger("3o14");
 
 const app = new Hono();
-app.use(federation(fedi, () => undefined))
+app.use(federation(fedi, () => undefined));
 
 app.get("/", (c) => c.text("Hello, Fedify!"));
+
+app.get("/setup", (c) => {
+  // same check as POST /setup here
+  const user = db.prepare<unknown[], User>("SELECT * FROM users LIMIT 1").get();
+  if (user != null) return c.redirect("/");
+
+  return c.html(
+    <Layout>
+      <SetupForm />
+    </Layout>,
+  );
+});
+
+app.post("/setup", async (c) => {
+  // NOTE for now only supports single user
+  // check if the account already exists (single user mode)
+  // TODO support multiusers
+  // maybe an option to select if the admin wants to make it a single user or
+  // multiusers and optimize for the selected options when setting things up
+  const user = db.prepare<unknown[], User>("SELECT * FROM users LIMIT 1").get();
+  if (user != null) return c.redirect("/");
+
+  const form = await c.req.formData();
+  const username = form.get("username");
+  if (typeof username !== "string" || !username.match(/^[a-z0-9_-]{1,50}$/)) {
+    return c.redirect("/setup");
+  }
+  db.prepare("INSERT INTO users (username) VALUES (?)").run(username);
+  return c.redirect("/");
+});
 
 export default app;
