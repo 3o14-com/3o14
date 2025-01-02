a963ba2fb22648184f0fe7db39d2ca3fb309a2d3
faultypointer <faultypointer@proton.me>
2025-01-02 20:50:51 +0545

    sending keys

--- 3o14/src/federation.ts
+++ 3o14/src/federation.ts
@@ -1,8 +1,15 @@
-import { createFederation, Endpoints, Person } from "@fedify/fedify";
+import {
+  createFederation,
+  Endpoints,
+  Person,
+  exportJwk,
+  generateCryptoKeyPair,
+  importJwk,
+} from "@fedify/fedify";
 import { getLogger } from "@logtape/logtape";
 import { InProcessMessageQueue, MemoryKvStore } from "@fedify/fedify";
 import db from "./db.ts";
-import type { Actor, User } from "./schema.ts";
+import type { Actor, User, Key } from "./schema.ts";
 
 const logger = getLogger("3o14");
 
@@ -13,32 +20,84 @@ const federation = createFederation({
 
 // return the user with specified username if available
 // on this instance
-federation.setActorDispatcher(
-  "/users/{identifier}",
-  async (ctx, identifier) => {
-    const user = db
-      .prepare<unknown[], User & Actor>(
-        `
+federation
+  .setActorDispatcher(
+    "/users/{identifier}",
+    async (ctx, identifier) => {
+      const user = db
+        .prepare<unknown[], User & Actor>(
+          `
           SELECT * FROM users
           JOIN actors ON (users.id = actors.user_id)
           WHERE users.username = ?
         `,
-      )
-      .get(identifier);
-    if (user == null) return null;
+        )
+        .get(identifier);
+      if (user == null) return null;
 
-    return new Person({
-      id: ctx.getActorUri(identifier),
-      preferredUsername: identifier,
-      name: user.name,
-      inbox: ctx.getInboxUri(identifier),
-      endpoints: new Endpoints({
-        sharedInbox: ctx.getInboxUri(),
-      }),
-      url: ctx.getActorUri(identifier),
-    });
-  },
-);
+      const keys = await ctx.getActorKeyPairs(identifier);
+      return new Person({
+        id: ctx.getActorUri(identifier),
+        preferredUsername: identifier,
+        name: user.name,
+        inbox: ctx.getInboxUri(identifier),
+        endpoints: new Endpoints({
+          sharedInbox: ctx.getInboxUri(),
+        }),
+        url: ctx.getActorUri(identifier),
+        publicKey: keys[0].cryptographicKey,
+        assertionMethods: keys.map((k) => k.multikey),
+      });
+    })
+  .setKeyPairsDispatcher(async (ctx, identifier) => {
+    const user = db
+      .prepare<unknown[], User>("SELECT * FROM users WHERE username = ?")
+      .get(identifier);
+    if (user == null) return [];
+
+    const rows = db
+      .prepare<unknown[], Key>("SELECT * FROM keys WHERE keys.user_id = ?")
+      .all(user.id);
+
+    const keys = Object.fromEntries(
+      rows.map((row) => [row.type, row]),
+    ) as Record<Key["type"], Key>;
+
+    const pairs: CryptoKeyPair[] = [];
+    for (const keyType of ["RSASSA-PKCS1-v1_5", "Ed25519"] as const) {
+      if (keys[keyType] == null) {
+        logger.debug(
+          "The user {identifier} does not have an {keyType} key; creating one...",
+          { identifier, keyType },
+        );
+        const { privateKey, publicKey } = await generateCryptoKeyPair(keyType);
+        db.prepare(
+          `
+            INSERT INTO keys (user_id, type, private_key, public_key)
+            VALUES (?, ?, ?, ?)
+          `,
+        ).run(
+          user.id,
+          keyType,
+          JSON.stringify(await exportJwk(privateKey)),
+          JSON.stringify(await exportJwk(publicKey)),
+        );
+        pairs.push({ privateKey, publicKey });
+      } else {
+        pairs.push({
+          privateKey: await importJwk(
+            JSON.parse(keys[keyType].private_key),
+            "private",
+          ),
+          publicKey: await importJwk(
+            JSON.parse(keys[keyType].public_key),
+            "public",
+          ),
+        });
+      }
+    }
+    return pairs;
+  });
 
 federation.setInboxListeners("/users/{identifier}/inbox", "/inbox");
 
