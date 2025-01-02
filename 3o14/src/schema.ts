08ab58f7f639efb3c682c8e50b7a6e02883ab75b
faultypointer <faultypointer@proton.me>
2025-01-02 20:50:42 +0545

    key interface

--- 3o14/src/schema.ts
+++ 3o14/src/schema.ts
@@ -14,3 +14,12 @@ export interface Actor {
   url: string | null; // url of actor's profile
   created: string; // time when actor account was created
 }
+
+// key pair for each actor for signing activities
+export interface Key {
+  user_id: number;
+  type: "RSASSA-PKCS1-v1_5" | "Ed25519";
+  private_key: string;
+  public_key: string;
+  created: string;
+}
