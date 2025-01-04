1d75f321cb733afbd7b645008972502f84287dd9
faultypointer <faultypointer@proton.me>
2025-01-04 11:32:06 +0545

    post dispatcher (Note)

--- 3o14/src/federation.ts
+++ 3o14/src/federation.ts
@@ -4,6 +4,7 @@ import {
   Follow,
   Note,
   Person,
+  PUBLIC_COLLECTION,
   Undo,
   createFederation,
   exportJwk,
@@ -14,8 +15,9 @@ import {
 } from "@fedify/fedify";
 import { getLogger } from "@logtape/logtape";
 import { InProcessMessageQueue, MemoryKvStore } from "@fedify/fedify";
+import { Temporal } from "@js-temporal/polyfill";
 import db from "./db.ts";
-import type { Actor, User, Key } from "./schema.ts";
+import type { Actor, Post, User, Key } from "./schema.ts";
 
 const logger = getLogger("3o14");
 
@@ -249,7 +251,28 @@ federation.setObjectDispatcher(
   Note,
   "/users/{identifier}/posts/{id}",
   (ctx, values) => {
-    return null;
+    const post = db
+      .prepare<unknown[], Post>(
+        `
+          SELECT posts.*
+          FROM posts
+          JOIN actors ON actors.id = posts.actor_id
+          JOIN users ON users.id = actors.user_id
+          WHERE users.username = ? AND posts.id = ?
+        `,
+      )
+      .get(values.identifier, values.id);
+    if (post == null) return null;
+    return new Note({
+      id: ctx.getObjectUri(Note, values),
+      attribution: ctx.getActorUri(values.identifier),
+      to: PUBLIC_COLLECTION,
+      cc: ctx.getFollowersUri(values.identifier),
+      content: post.content,
+      mediaType: "text/html",
+      published: Temporal.Instant.from(`${post.created.replace(" ", "T")}Z`),
+      url: ctx.getObjectUri(Note, values),
+    });
   },
 );
 
